65a577411c6248f3908fdc21c3148f3349f23fca
hany66 <hanyshaker@hotmail.com>
2025-02-23 22:20:38 +0200

    Update error codes

--- movies_api.js
+++ movies_api.js
@@ -60,8 +60,9 @@ async function scrapeNews() {
         const data = await scrapeNews(category);
         res.json({ success: 'ok', articles: data });
     } catch (error) {
-        console.error("Error:", error.message);
-        // res.status(500).json({ error: 'Something went wrong!'||error });
+       // console.error("Error:", error.message);
+         // res.status(500).json({ error: 'Something went wrong!' });
+        return res.status(500).json({error: "Internal Server Error",details: error.message, });
        // res.status(400).json({ error: 'Not Found!' });
        // res.status(200).json({ error: 'code 200 !' });
        // console.log(https.STATUS_CODES);
