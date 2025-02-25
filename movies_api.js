73c294671ea5b81462a8c31e75688a54f2dcf4af
Hany Shaker <hanyshaker@tanmia.local>
2025-02-25 09:59:29 +0200

    fix package.json

--- movies_api.js
+++ movies_api.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const http = require("https");
+const http = require("http");
 const https = require("https");
-
 const app = express();
-//const PORT = 3000;
 const PORT = process.env.PORT || 3000; // Use the environment variable, fallback to 3000 for local dev
-
 // Function to scrape news from any category
 async function scrapeNews() {
    // const browser = await puppeteer.launch();
@@ -19,19 +16,7 @@ async function scrapeNews() {
     //const url = `https://www.themoviedb.org/movie`;
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-    /*const articles = await page.evaluate(() => {
-        const elements = document.querySelectorAll('.card.style_1');
-        return Array.from(elements).map(el => ({
-            title: el.querySelector('.content h2 a')?.textContent.trim() || 'No title',
-            rating: el.querySelector('.user_score_chart')?.getAttribute('data-percent') || 'No rating',
-            url: el.querySelector('.content h2 a') ? 'https://www.themoviedb.org' + el.querySelector('.content h2 a').getAttribute('href') : null,
-             img: el.querySelector('.wrapper.glyphicons_v2.picture.grey.no_image_holder  img')?.getAttribute('src') || 'No image',
-             time: el.querySelector('.content p')?.textContent || 'No timestamp'
-
-        }));
-
-    });*/
-    const articles = await page.evaluate(() => {
+       const articles = await page.evaluate(() => {
         const elements = document.querySelectorAll('.card.style_1');
         return Array.from(elements)
             .map(el => {
@@ -46,31 +31,22 @@ async function scrapeNews() {
             })
             .filter(article => article !== null); // Remove null values
     });
-
     await browser.close();
-
     return articles;
 }
-
-// Express Route to Serve Scraped Data
-//app.get('/movie', async (req, res) => {
     app.get('/', async (req, res) => {
     try {
         const { category } = req.params;
         const data = await scrapeNews(category);
         res.json({ success: 'ok', articles: data });
     } catch (error) {
-        // console.error("Error:", error.message);
+
          // res.status(500).json({ error: 'Something went wrong!' });
        // return res.status(500).json({error: "Internal Server Error",details: error.message, });
           res.status(400).json({ error: 'Not Found!' });
-      //   res.status(200).json({ error: res });
-       // console.log(https.STATUS_CODES);
 
-          //res.statusCodes;
     }
 });
-
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
