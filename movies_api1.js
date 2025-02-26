3e660e75c91cb2ec64aa582414cead2565ec4d82
hany66 <hanyshaker@hotmail.com>
2025-02-26 22:06:12 +0200

    Update error....

--- movies_api1.js
+++ movies_api1.js
@@ -11,8 +11,8 @@ app.use(express.json());
 // Function to scrape movies from TMDb
 async function scrapeMovies() {
     const browser = await puppeteer.launch({
+       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH ,
         headless: 'new', // Latest headless mode
-       // executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-web-security']
     });
 
